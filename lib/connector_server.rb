1d1a2038ff2b2b18932c3b48f1230de479d3914d
Dezeray Kowalski <dkowalski@mavenlink.com>
2020-07-16 16:47:55 -0600

    create a Grpc Action Request and implement performing the action
    Co-authored-by: Ethan Anderson <eanderson@mavenlink.com>

--- lib/connector_server.rb
+++ lib/connector_server.rb
@@ -1,17 +1,53 @@
 require_relative "connector_service_definition/connector_services_pb"
+require 'json'
+require 'net/http'
+require 'openssl'
 
 class ConnectorServer < Grpc::Connector::Service
   def triggers(triggers_req, _unused_call)
     Grpc::TriggersResponse.new(triggers: [])
   end
 
-  def actions(actions_req, _unused_call)
-    Grpc::ActionsResponse.new(actions: [])
+  def actions(actions_request, _unused_call)
+    actions = [
+      Grpc::Action.new(display_name: "Close Project", description: "Close a project by archiving it", inputs: [
+        Grpc::Field.new(display_name: "Project ID", key: "project_id", type: "text", description: "The ID of the project to be closed.", required: true),
+        Grpc::Field.new(display_name: "App Account", key: "target", type: "app_account", description: "The Mavenlink app account for which to send the payload", required: true)
+      ], outputs: [])
+    ]
+
+    Grpc::ActionsResponse.new(actions: actions)
   end
 
   def perform_action(action_request, _unused_call=nil)
+    app_account = JSON.parse(action_request.params["target"])
+    project_id = action_request.params["project_id"]
+    archive_project_from_id(app_account["token"], project_id)
   end
 
   def perform_trigger(trigger_request, _unused_call=nil)
+  end
+
+  private
+
+  def archive_project_from_id(token, project_id)
+    begin
+      authorization = "Bearer #{token}"
+      url = URI("https://api.msync.mvn.link/api/v1/workspaces/#{project_id}")
+      body = {"workspace": {"archived": true}}.to_json
+
+      https = Net::HTTP.new(url.host, url.port);
+      https.use_ssl = true
+      request = Net::HTTP::Put.new(url)
+      request["Authorization"] = authorization
+      request["Content-Type"] = "application/json"
+      request.body = body
+
+      response = https.request(request)
+
+      Grpc::ActionResponse.new(status: :SUCCESS, outputs: {})
+    rescue => error
+      Grpc::ActionResponse.new(status: :ERROR, outputs: {error: "Slack API Error"})
+    end
   end
 end
--- spec/connector_server_spec.rb
+++ spec/connector_server_spec.rb
@@ -1,10 +1,16 @@
 require_relative '../lib/connector_server'
+require 'spec_helper'
 
 describe ConnectorServer do
     describe '#actions' do
         it "returns an empty array" do 
             actions_request = Grpc::ActionsRequest.new()
-            expect(described_class.new.actions(actions_request, {})).to eq Grpc::ActionsResponse.new(actions: [])
+            expect(described_class.new.actions(actions_request, {}).actions).to eq (
+                    [Grpc::Action.new(display_name: "Close Project", description: "Close a project by archiving it", inputs: [
+                            Grpc::Field.new(display_name: "Project ID", key: "project_id", type: "text", description: "The ID of the project to be closed.", required: true),
+                            Grpc::Field.new(display_name: "App Account", key: "target", type: "app_account", description: "The Mavenlink app account for which to send the payload", required: true)],
+                        outputs: [])]
+            )
         end
     end
 
@@ -16,9 +22,31 @@ describe ConnectorServer do
     end
 
     describe '#perform_action' do
-        it 'exists' do
-            action_request = Grpc::ActionRequest.new()
-            described_class.new.perform_action(action_request)
+        it 'it can close a project' do
+
+            stub_request(:put, "https://api.msync.mvn.link/api/v1/workspaces/123").
+            with(
+              body: "{\"workspace\":{\"archived\":true}}",
+              headers: {
+                'Accept'=>'*/*',
+                'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                'Authorization'=>'Bearer whatever',
+                'Content-Type'=>'application/json',
+                'Host'=>'api.msync.mvn.link',
+                'User-Agent'=>'Ruby'
+              }).
+            to_return(status: 200, body: "", headers: {})
+
+            action_request = Grpc::ActionRequest.new(action:
+                Grpc::Action.new(display_name: "Close Project"), params:
+                  {
+                    "target" => {token: "whatever"}.to_json,
+                    "project_id" => "123"
+                  }
+              )
+            response = described_class.new.perform_action(action_request)
+            expect(response.status).to eq :SUCCESS
+            expect(response.outputs).to eq({})
         end
     end
 
